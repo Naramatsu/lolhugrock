92e7a44af4215a84882bb2c9438e54da96ae2876
Narmatsu <joncyy_19@hotmail.com>
2023-08-08 19:44:42 -0500

    added footer component

--- src/components/BoostingSection/BoostingSection.jsx
+++ src/components/BoostingSection/BoostingSection.jsx
@@ -11,52 +11,58 @@ import {
   formPreferences,
 } from "./data";
 import "./BoostingSection.style.scss";
+import Footer from "../Footer/Footer";
+import OrderSummary from "../OrderSummary/OrderSummary";
 
 const BoostingSection = ({ title, color }) => {
   const colorFormatted = convertColor(color);
   return (
-    <BackgroundAnimated color={colorFormatted}>
-      <section className="boosting__section">
-        <h2 style={{ color: colorFormatted }}>{title}</h2>
-        <GridLayout template="2fr 1fr">
-          <GridLayout template="1fr 1fr">
-            {formPreferences.map((form, index) => (
-              <PreferencePanel
-                key={index}
-                type={form.type}
-                title={form.title}
-                formName={form.name}
-                form={form}
-                color={colorFormatted}
-                shape={form.shape}
-              />
-            ))}
-            <GridLayout template="3fr" gridColumn="span 2">
-              <PreferencePanel
-                type={formAdditionalPreferences.type}
-                title={formAdditionalPreferences.title}
-                formName={formAdditionalPreferences.name}
-                form={formAdditionalPreferences}
-                color={colorFormatted}
-              />
+    <>
+      <BackgroundAnimated color={colorFormatted}>
+        <section className="boosting__section">
+          <h2 style={{ color: colorFormatted }}>{title}</h2>
+          <GridLayout template="2fr 1fr">
+            <GridLayout template="1fr 1fr">
+              {formPreferences.map((form, index) => (
+                <PreferencePanel
+                  key={index}
+                  type={form.type}
+                  title={form.title}
+                  formName={form.name}
+                  form={form}
+                  color={colorFormatted}
+                  shape={form.shape}
+                />
+              ))}
+              <GridLayout template="3fr" gridColumn="span 2">
+                <PreferencePanel
+                  type={formAdditionalPreferences.type}
+                  title={formAdditionalPreferences.title}
+                  formName={formAdditionalPreferences.name}
+                  form={formAdditionalPreferences}
+                  color={colorFormatted}
+                />
+              </GridLayout>
+              <GridLayout template="3fr" gridColumn="span 2">
+                <Panel title="Boosting Rank" color={colorFormatted}>
+                  {formBoostingRank.map((form, index) => (
+                    <CardDivision
+                      key={index}
+                      title={form.title}
+                      formName={form.name}
+                      label={form.label}
+                      items={form.items}
+                    />
+                  ))}
+                </Panel>
+              </GridLayout>
             </GridLayout>
-            <GridLayout template="3fr" gridColumn="span 2">
-              <Panel title="Boosting Rank" color={colorFormatted}>
-                {formBoostingRank.map((form, index) => (
-                  <CardDivision
-                    key={index}
-                    title={form.title}
-                    formName={form.name}
-                    label={form.label}
-                    items={form.items}
-                  />
-                ))}
-              </Panel>
-            </GridLayout>
+            <OrderSummary />
           </GridLayout>
-        </GridLayout>
-      </section>
-    </BackgroundAnimated>
+        </section>
+      </BackgroundAnimated>
+      <Footer color={colorFormatted} />
+    </>
   );
 };
 
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Footer = ({ color }) => (
+  <footer style={{ background: color }}>
+    <p>Todos los derechos reservados</p>
+  </footer>
+);
+
+export default Footer;
