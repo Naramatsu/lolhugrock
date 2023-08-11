c960105699e52ce3334810773494d202fae6407e
Narmatsu <joncyy_19@hotmail.com>
2023-08-11 15:59:12 -0500

    added dynamic color on path change

--- src/components/Panel/Panel.jsx
+++ src/components/Panel/Panel.jsx
@@ -2,13 +2,11 @@ import React from "react";
 import "./Panel.style.scss";
 
 const Panel = ({ title, color, children }) => (
-  <section className={`preference__panel`}>
+  <section className={`preference__panel`} style={{ "--cl": color }}>
     <p className="preference__panel__title" style={{ background: color }}>
       {title}
     </p>
-    <section className="form__container" style={{ "--cl": color }}>
-      {children}
-    </section>
+    <section className="form__container">{children}</section>
   </section>
 );
 
