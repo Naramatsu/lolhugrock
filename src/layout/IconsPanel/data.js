d6fee98fc7dabcbe2b8ec43118b444748c22e7a4
Narmatsu <joncyy_19@hotmail.com>
2023-11-21 16:11:46 -0500

    added iconsPanel list

---
+++ src/layout/IconsPanel/data.js
@@ -0,0 +1,13 @@
+import fbIcon from "../../assets/facebook-logo.png";
+import wapIcon from "../../assets/wap_icon.png";
+import igIcon from "../../assets/instagram-icon.png";
+import ytIcon from "../../assets/youtube-icon.png";
+import dcIcon from "../../assets/discord-logo.png";
+
+export const iconList = [
+  { img: fbIcon, link: "" },
+  { img: igIcon, link: "" },
+  { img: ytIcon, link: "" },
+  { img: dcIcon, link: "" },
+  { img: wapIcon, link: "" },
+];
