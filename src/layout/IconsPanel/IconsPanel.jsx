0324ec6e26ae3b94279a08c9c1ea4aef99b61ff1
Narmatsu <joncyy_19@hotmail.com>
2023-11-30 17:43:16 -0500

    updated IconPanel component

--- src/layout/IconsPanel/IconsPanel.jsx
+++ src/layout/IconsPanel/IconsPanel.jsx
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { iconList } from "./data";
+import { BsPlusCircle } from "react-icons/bs";
 import "./IconsPanel.style.scss";
 
-const IconsPanel = () => (
-  <section className="float__icons__panel">
-    {iconList.map(({ img, link }, index) => (
-      <a href={link} target="_blank" rel="noreferrer">
-        <img key={index} src={img} alt="icon" />
-      </a>
-    ))}
-  </section>
-);
+const IconsPanel = () => {
+  const [togglePanel, setTogglePanel] = useState(false);
+  const isPanelOpen = togglePanel ? "open" : "close";
+  return (
+    <>
+      <BsPlusCircle
+        className={`icon-plus ${isPanelOpen}`}
+        onClick={() => setTogglePanel(!togglePanel)}
+      />
+      <section className={`float__icons__panel ${isPanelOpen}`}>
+        {iconList.slice(0, 3).map(({ img, link }, index) => (
+          <a key={index} href={link} target="_blank" rel="noreferrer">
+            <img src={img} alt="icon" />
+          </a>
+        ))}
+        <section className="divisor" />
+        {iconList.slice(3, iconList.length).map(({ img, link }, index) => (
+          <a key={index} href={link} target="_blank" rel="noreferrer">
+            <img src={img} alt="icon" />
+          </a>
+        ))}
+      </section>
+    </>
+  );
+};
 
 export default IconsPanel;
--- src/layout/IconsPanel/data.js
+++ src/layout/IconsPanel/data.js
@@ -3,11 +3,13 @@ import wapIcon from "../../assets/wap_icon.png";
 import igIcon from "../../assets/instagram-icon.png";
 import ytIcon from "../../assets/youtube-icon.png";
 import dcIcon from "../../assets/discord-logo.png";
+import xIcon from "../../assets/x-logo.png";
 
 export const iconList = [
   { img: fbIcon, link: "" },
   { img: igIcon, link: "" },
   { img: ytIcon, link: "" },
   { img: dcIcon, link: "" },
+  { img: xIcon, link: "" },
   { img: wapIcon, link: "" },
 ];
